875fc3e996087cc756a2a8f86484c621d140da48
Ted Airey <ted.airey4@gmail.com>
2020-05-05 23:07:33 -0400

    Updated How It Works page and added scss variable

--- src/howItWorks/howItWorks.js
+++ src/howItWorks/howItWorks.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './howItWorks.scss';
 import { Link } from 'react-router-dom';
 import { scrollTop } from '../Helpers';
+import { Carousel } from 'react-bootstrap';
 
 function HowItWorks() {
     return (
@@ -13,45 +14,86 @@ function HowItWorks() {
                 <iframe className="col-md-7" src="https://player.vimeo.com/video/357418464?transparent=0&amp;app_id=122963" width="695" height="460" id="meg-campaign-video" data-ready="true" tabIndex="-1"></iframe>
             </div>
             <hr/>
+            <div className="carousel-header">
+                <h3>Based on proven science</h3>
+            </div>
+            <Carousel interval={7000}>
+                <Carousel.Item>
+                    <img
+                    className="image-slide"
+                    src="https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_limit,w_695/v1568420562/emokuhietha0cpmkpsb2.jpg"
+                    alt="First slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="image-slide"
+                    src="https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_limit,w_695/v1568420581/w5caqdwhkpwes67cropr.jpg"
+                    alt="Second slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="image-slide"
+                    src="https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_limit,w_695/v1568420600/kboij9aomukb7n1hnddw.jpg"
+                    alt="Third slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="image-slide"
+                    src="https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_limit,w_695/v1568420618/kq9y3mpvykgrr8qlt4sb.jpg"
+                    alt="Fourth slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="image-slide"
+                    src="https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_limit,w_695/v1568420636/apasok2m8cfktabtssuj.jpg"
+                    alt="Fifth slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="image-slide"
+                    src="https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_limit,w_695/v1568420651/gzrzupnilm4sskrczjv0.jpg"
+                    alt="Sixth slide"
+                    />
+                </Carousel.Item>
+            </Carousel>
+            <hr/>
+            <h2 className='user-feedback-header'>
+                What do our users say?
+            </h2>
             <div className="row section">
-                <img className="col-md-7" src="https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_limit,w_695/v1568420562/emokuhietha0cpmkpsb2.jpg"/>
+                <img className="col-md-7" src="https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_limit,w_695/v1568751667/hu5qy2jdfz5ssxfio5uk.png"/>
                 <p className="col-md-5">
-                    Section 1
+                    <span className='quote'>"Keeps Me Motivated"</span><br/>
+                    <span className='person'>-Alex K.</span>
                 </p>
             </div>
             <hr/>
             <div className="row section">
-                <img className="col-md-7" src="https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_limit,w_695/v1568420581/w5caqdwhkpwes67cropr.jpg"/>
+                <img className="col-md-7" src="https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_limit,w_695/v1568750847/okwx41mw4cxswgya9qy3.png"/>
+                <div className="col-md-5">
+                    <span className='quote'>"The Perfect Friend"</span><br/>
+                    <span className='person'>-Roberta P.</span>
+                </div>
+            </div>
+            <hr/>
+            <div className="row section">
+                <img className="col-md-7" src="https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_limit,w_695/v1568751231/my3agepuhv8xgksuje18.png"/>
                 <p className="col-md-5">
-                    Section 2
+                    <span className='quote'>"All the Benefits of Dietitian Input"</span><br/>
+                    <span className='person'>-Pat B.</span>
                 </p>
             </div>
             <hr/>
             <div className="row section">
-                <img className="col-md-7" src="https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_limit,w_695/v1568420600/kboij9aomukb7n1hnddw.jpg"/>
+                <img className="col-md-7" src="https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_limit,w_695/v1568751418/lpqdqxovwg7awscq2h6g.png"/>
                 <p className="col-md-5">
-                    Section 3
-                </p>
-            </div>
-            <hr/>
-            <div className="row section">
-                <img className="col-md-7" src="https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_limit,w_695/v1568420618/kq9y3mpvykgrr8qlt4sb.jpg"/>
-                <p className="col-md-5">
-                    Section 4
-                </p>
-            </div>
-            <hr/>
-            <div className="row section">
-                <img className="col-md-7" src="https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_limit,w_695/v1568420636/apasok2m8cfktabtssuj.jpg"/>
-                <p className="col-md-5">
-                    Section 5
-                </p>
-            </div>
-            <hr/>
-            <div className="row section">
-                <img className="col-md-7" src="https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_limit,w_695/v1568420651/gzrzupnilm4sskrczjv0.jpg"/>
-                <p className="col-md-5">
-                    Section 6
+                    <span className='quote'>"So Nice"</span><br/>
+                    <span className='person'>-Maggie A.</span>
                 </p>
             </div>
             <hr/>
